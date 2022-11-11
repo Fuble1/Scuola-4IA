1b8e43668d98764fa82c9daabebbe4221e267526
Fuble1 <93370355+Fuble1@users.noreply.github.com>
2022-11-11 12:28:12 +0100

    Add files via upload

--- C/UD 2/p02.c
+++ C/UD 2/p02.c
@@ -1,3 +1,17 @@
+/*
+Programma per la gestione di una stringa inserita da tastiera (stringa allocata massimo 100byte), utilizzando un puntatore a stringa.
+
+    - initStringa() Richiede l'input di una stringa da tastiera.
+    - printStringa() Stampa la stringa passata.
+    - getStrLength() Ritorna la lunghezza della stringa passata.
+    - printReversedString Stampa la stringa al contrario.
+    - stampaStringaRicorsiva() stampa una stringa in maniera ricorsiva.
+    - stampaStringaReversedRicorsiva() stampa una stringa al contrario in modo ricorsivo.
+    - getCharAt() Ritorna il carattere della stringa alla posizione scelta; -1 se la posizione non esiste.
+    - countChar() Conta e restituisce il numero di occorrenze di un carattere in una stringa.
+    - checkPalindroma() Verifica si una parola è palindroma oppure no; 1 Vero 0 Falso.
+ */
+
 #include <stdio.h>
 #include <stdlib.h>
 
