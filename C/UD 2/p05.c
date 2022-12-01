7b8be8de3fd73ce06c51fad3fd8792e5c2389735
Fuble1 <93370355+Fuble1@users.noreply.github.com>
2022-12-01 12:33:48 +0100

    Add files via upload

---
+++ C/UD 2/p05.c
@@ -0,0 +1,11 @@
+/*
+    Realizzare un programma per la gestione di stringhe, allocazione iniziale 100byte, utilizzando puntatore a stringa.
+    -checkPalindroma() Verifica si una stringa è palindroma.
+    -checkAnagramma() Verifica se le due stringhe passate sono due anagrammi.
+    -printAnagrammi() Riceve una stringa ed il numero di anagrammi da generare quindi stampa il numero di anagrammi richiesto.
+    -checkSubstring() Riceve una stringa ed una sottostringa, verifica se sottostringa è presente nella stringa principale.
+    Anagramma: Stringa2 è anagramma di Stringa1 se è composta dalle stesse lettere ma in posizioni diverse. es: tosto -> sotto
+*/
+
+#include <stdio.h>
+#include <stdlib.h>
