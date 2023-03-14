e424e04a47e584ac6988ea996c6af386cf34cd20
Fuble1 <93370355+Fuble1@users.noreply.github.com>
2023-03-14 12:41:20 +0100

    bo

---
+++ TPSI/Librerie/e01/lib.h
@@ -0,0 +1,7 @@
+// File dedicato ai prototipi delle funzioni
+
+/**
+ * Funzione che stampa un numero
+ * @param int numero da stampare
+ */
+void f(int);
---
+++ TPSI/Librerie/e01/main.c
@@ -0,0 +1,7 @@
+#include <stdio.h>
+#include "lib.c"
+
+int main(){
+    f(18);
+    return 0;
+}
