1b2521b6fdfa7f0c2bdc7450b668ea2252f1dc05
Fuble1 <93370355+Fuble1@users.noreply.github.com>
2023-03-31 12:05:25 +0200

    Add files via upload

--- C/UD 4/p01.c
+++ C/UD 4/p01.c
@@ -8,10 +8,62 @@ typedef struct nodo{
 }Nodo;
 
 // Dichiarazione dei prototipi
+
+/**
+ * @brief Aggiunge un nodo in testa alla lista
+ * @param Nodo* Puntatore alla testa della lista.
+ * @param int Valore da inserire nel nodo.
+ * @return Nuova testa della lista.
+*/
 Nodo* addTesta(Nodo *, int);
+
+/**
+ * @brief Visualizza una lista.
+ * @param Nodo* Puntatore alla testa della lista.
+*/
 void showLista(Nodo *);
+
+/**
+ * @brief Visualizza una lista in modo ricorsivo
+ * @param Nodo* Puntatore alla lista.
+*/
 void showListaRecursive(Nodo *);
+
+/**
+ * @brief Visualizza una lista al contrario in modo ricorsivo.
+ * @param Nodo* Puntatore alla lista.
+*/
 void showListaReversedRecursive(Nodo *);
+
+/**
+ * @brief Estrae un nodo dalla testa della lista, con controllo esistenza.
+ * @param Nodo* Puntatore alla testa della lista
+ * @return Nuova testa della lista.
+*/
+Nodo* popTesta(Nodo *);
+
+/**
+ * @brief Aggiunge un nodo alla coda della lista.
+ * @param Nodo* Puntatore alla testa della lista.
+ * @param int valore da inserire nel nodo
+ * @return Nodo* Puntatore alla nuova testa.
+*/
+Nodo* pushCoda(Nodo *, int );
+
+/**
+ * @brief Ricerca il valore massimo o minimo interno alla lista.
+ * @param Nodo* Puntatore alla testa della lista.
+ * @param int 0->Minimo; 1->Massimo
+ * @return Valore trovato.
+*/
+int ricercaMaxMinLista(Nodo *, int);
+
+/**
+ * @brief Determina la lunghezza di una lista.
+ * @param Nodo* Puntatore alla testa della lista.
+ * @return Numero di nodi che compongono la lista.
+*/
+int contaNodi(Nodo*);
 
 // ### MAIN PROOGRAM ###
 int main(){
@@ -62,9 +114,74 @@ void showListaRecursive(Nodo *_tmp){
         showListaRecursive(_tmp->next);
     }
 }
+
 void showListaReversedRecursive(Nodo *_tmp){
     if(_tmp != NULL){
         showListaReversedRecursive(_tmp->next);
         printf("Address: %d; val: %d, next: %d\n", _tmp, _tmp->val, _tmp->next);        
     }
+}
+
+Nodo* popTesta(Nodo *_head){
+	if(_head == NULL)
+		return NULL;
+	Nodo* _nh = _head->next;	// New head
+	free(_head);
+	return _nh;
+}
+
+Nodo* pushCoda(Nodo *_head, int _val){
+	Nodo *_tmp = _head;	// Nodo temporaneo di appoggio per scorrere lista
+	Nodo *_nt = (Nodo*) malloc(sizeof(Nodo));	// New tail
+	_nt->next = NULL;
+	_nt->val = _val;
+	
+	if(_tmp == NULL)	// Se la lista era vuota, ritorno il nuovo nodo
+		return _nt;
+	
+	//getTail
+	while ( _tmp->next != NULL){
+		_tmp = _tmp->next;
+	}
+	_tmp->next = _nt;
+	return _head;
+}
+
+int ricercaMaxMinLista(Nodo *_head, int _scelta){
+	// scelta == 0, minimo
+	// scelta == 1, massimo
+	if(_head != NULL){
+		if(_scelta == 1){		// ricerca massimo
+			int max = _head->val;
+			Nodo *_tmp = _head;
+			while ( _tmp != NULL){	// scorro lista
+				if(_tmp->val > max )	// aggiorno massimo
+					max = _tmp->val;
+				_tmp = _tmp->next;
+			}
+			return max;
+		}
+		if(_scelta == 0){		// ricerca minore
+			int min = _head->val;
+			Nodo *_tmp = _head;
+			while ( _tmp != NULL){	// scorro lista
+				if(_tmp->val < min )	// aggiorno minimo
+					min = _tmp->val;
+				_tmp = _tmp->next;
+			}
+			return min;
+		}
+		return -1;	// se parametro errato
+	}
+	return -1;		// se lista vuota
+}
+
+int contaNodi(Nodo* _head){
+	int cnt = 0;	// numero nodi
+	Nodo* _tmp = _head;	// nodo tmp per scorrere
+	while ( _tmp != NULL){			// scorro lista
+		cnt++;
+		_tmp = _tmp->next;
+	}
+	return cnt;		// ritorno numero nodi
 }
